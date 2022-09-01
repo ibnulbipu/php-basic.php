9aa8aa52fd0e8ce580ae5f7610ed23b945e01d37
ibnulbipu <112407362+ibnulbipu@users.noreply.github.com>
2022-09-01 11:31:58 +0600

    Make Even and Odd number loop functions

---
+++ Even_Odd.php
@@ -0,0 +1,21 @@
+<?php 
+
+echo "The Even Number <br>";
+$y=2;
+do {
+  echo "$y <br>";
+  $y+=2;
+} while ($y < 100);
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+$x = 1;
+
+echo "The Odd Number <br>";
+do {
+  echo "$x <br>";
+  $x+=2;
+} while ($x < 100);
